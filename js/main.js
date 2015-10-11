aec2dd9c593c81d7d11fa4cb0ac1669bb7742c22
berthamz <berthamz@gmail.com>
2015-10-11 14:13:26 -0700

    Structure, compass files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 jQuery(document).ready(function(){
+
 	$(function(){
 	  $("#header").load("header.html"); 
 	  $("#footer").load("footer.html"); 
 	});
+
+	$(window).scroll(function() {
+	if ($(this).scrollTop() > 50){  
+	    $('#header').addClass("sticky");
+	  }
+	  else{
+	    $('#header').removeClass("sticky");
+	  }
+	});
+
 });
